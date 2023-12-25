f6a48af36a8ce3ac0302f0db44bde9a08c7324e8
Kisa <55666909+Kisa04@users.noreply.github.com>
2023-12-25 21:00:23 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+//let a;
+document.addEventListener('DOMContentLoaded', function() {
+
+const worker = new Worker('worker.js');
+worker.postMessage('start');
+
+    
+    
+worker.addEventListener("message", function (e) {
+        window.getKaiAd({
+            publisher: "0b7a2f6a-4113-4b9d-bce7-e3b0ba119873",
+            app: "whatsapp",
+            slot: "whatsapp",
+            h: 80,
+            w: 240,
+            container: document.getElementById('ads'),
+            onerror: function (err) {
+                console.error("Custom catch:", err);
+            },
+            onready: function (ad) {
+                ad.call("display", {
+                    tabindex: 0,
+                    navClass: "items",
+                    display: "block"
+                });
+                ad.on("click", function () {
+                    console.log("click event");
+                });
+            }
+        });
+    });
+document.addEventListener("keydown", (function(e) {
+            switch (e.key) {
+                case "Enter":
+                    var n = document.getElementsByClassName("items")[document.getElementsByClassName("items").length - 1];
+                n.focus();
+                    break;
+            }
+        }))
+
+// Function to generate QR code
+        function generateQRCode(text) {
+            // Create a QR code element
+            const qrCodeElement = document.createElement('img');
+            qrCodeElement.src = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(text)}&size=240x240`;
+
+            // Append the QR code element to the body
+            document.body.appendChild(qrCodeElement);
+        }
+	let a;
+        // Function to check if the response includes "api.whatsapp.com"
+        function checkResponse(timestamp) {
+            const url = `https://wpp-connect.fly.dev/wpp/get/${timestamp}`;
+		
+            // Make a fetch request
+            fetch(url)
+                .then(response =>  response.text())
+  		.then(responseText => {
+                    if (responseText.includes("api.whatsapp.com")) {
+                        const whatsappLink = responseText.replace('+', '%2B');
+                        //window.location.href = whatsappLink;
+			
+			if (a && !a.closed) {
+    				console.log('The window is open.');
+				a.close();
+				setTimeout(function() {
+				a = window.open(whatsappLink);
+				}, 3000);
+			    console.log("done");
+			    checkResponse(timestamp);
+			} else {
+			    a = window.open(whatsappLink);
+    			    //setTimeout(function() {
+  			    //a.close();
+			    //}, 20000);
+			    console.log("done");
+			    checkResponse(timestamp);
+			}
+			
+                
+	    
+
+                    } else {
+                        // If the response does not include "api.whatsapp.com", retry after 5 seconds
+                        setTimeout(() => checkResponse(timestamp), 5000);
+                    }
+                })
+                .catch(error =>{
+		 console.error("Error:", error);
+		 checkResponse(timestamp);
+		});
+        }
+
+        // Get the current timestamp in Unix format
+        const currentTimestamp = Math.floor(new Date().getTime() / 1000);
+
+        // Generate QR code and start checking for the response
+        const url = `https://wpp-connect.fly.dev/whatsapp/${currentTimestamp}`;
+        generateQRCode(url);
+        checkResponse(currentTimestamp);
+})
---
+++ worker.js
@@ -0,0 +1,8 @@
+self.postMessage('loadMorePins');
+self.addEventListener('message', function (e) {
+  if (e.data === 'start') {
+    setInterval(function () {
+      self.postMessage('loadMorePins');
+    }, 10000);
+  }
+});
